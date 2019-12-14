1d9d7781672cda0b435e56a008ef213bb8e73d72
jigglypop <ydh2244@gmail.com>
2019-12-14 21:34:48 +0900

    1214

---
+++ 05_회귀/연습장.py
@@ -0,0 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(0)
+X = 2*np.random.rand(100,1)
+
--- 사용자행동인식.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-%matplotlib inline
-feature_name_df = pd.read_csv('E:/data/human_activity/features.txt')
