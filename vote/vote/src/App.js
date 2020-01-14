f72daeff337bc3879ba0aaa8437401de30cf5717
jigglypop <ydh2244@gmail.com>
2020-01-14 21:28:55 +0900

    0114

---
+++ vote/vote/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./sections/Header";
+import PersistentDrawerLeft from "./sections/Layout";
+import Footer from "./sections/Footer";
+
+function App() {
+  return (
+    <>
+      <PersistentDrawerLeft />
+    </>
+  );
+}
+
+export default App;
---
+++ vote/vote/src/components/B.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const B = () => {
+  return (
+    <>
+      <h1>B</h1>
+    </>
+  );
+};
+export default B;
